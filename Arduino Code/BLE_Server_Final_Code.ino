e36bcc558e949dfa0f8c9ecfdad534d599be1673
Dino Mintu <guiganol@gmail.com>
2024-02-19 07:45:28 +0900

    Added Arduino Code

---
+++ Arduino Code/BLE_Server_Final_Code.ino
@@ -0,0 +1,129 @@
+/*
+  Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+  Ported to Arduino ESP32 by Evandro Copercini
+  updated by chegewara and MoThunderz
+*/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+BLECharacteristic* pCharacteristic_2 = NULL;
+BLEDescriptor *pDescr;
+BLE2902 *pBLE2902;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHAR1_UUID          "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHAR2_UUID          "e3223119-9445-4e96-a4a1-85358c4046a2"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class CharacteristicCallBack: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pChar) override { 
+    uint8_t* pChar2Data = pChar->getData();
+    uint8_t valueReceived = pChar2Data[0];
+    uint8_t rValue = pChar2Data[1];
+    uint8_t gValue = pChar2Data[2];
+    uint8_t bValue = pChar2Data[3];
+    Serial.printf("rValue: %i\n gValue: %i\n bValue: %i\n", rValue, gValue, bValue);
+    Serial.println(valueReceived);
+    if (valueReceived == 1) {
+      neopixelWrite(RGB_BUILTIN, rValue, gValue, bValue);
+      //digitalWrite(RGB_BUILTIN, HIGH);
+    } else {
+      digitalWrite(RGB_BUILTIN, LOW);
+    }
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHAR1_UUID,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );                   
+
+  pCharacteristic_2 = pService->createCharacteristic(
+                      CHAR2_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  
+                    );  
+
+  // Create a BLE Descriptor
+  
+  pDescr = new BLEDescriptor((uint16_t)0x2901);
+  pDescr->setValue("A very interesting variable");
+  pCharacteristic->addDescriptor(pDescr);
+  
+  pBLE2902 = new BLE2902();
+  pBLE2902->setNotifications(true);
+  
+  // Add all Descriptors here
+  pCharacteristic->addDescriptor(pBLE2902);
+  pCharacteristic_2->addDescriptor(new BLE2902());
+  
+  // After defining the desriptors, set the callback functions
+  pCharacteristic_2->setCallbacks(new CharacteristicCallBack());
+  
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+    // notify changed value
+    if (deviceConnected) {
+        pCharacteristic->setValue(value);
+        pCharacteristic->notify();
+        value++;
+        delay(1000);
+    }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+}
